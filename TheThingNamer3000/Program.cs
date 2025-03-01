4b136d178af316b790e07669d98eb1ad62b9e36c
Justin Beam <justinbeam@MacBookAir.attlocal.net>
2025-03-01 11:06:21 -0600

    did project

--- TheThingNamer3000/Program.cs
+++ TheThingNamer3000/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // NOUN
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // ADJUCTIVE
+string c = " of Doom"; 
+string d = " 3000";
+Console.WriteLine("The " + b + " " + a + c + d + "!");
+/* 
+ The ADJUCTIVE NOUN of Doom 3000
+*/
