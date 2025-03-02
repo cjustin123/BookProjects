3377b2301287e074bf29aa6abbf88b7596b6ae7c
Justin Beam <justinbeam@MacBookAir.attlocal.net>
2025-03-02 09:11:13 -0600

    started watchtower. tweaking UI

---
+++ Watchtower/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("X value: ");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+int _xValue = Convert.ToInt32(Console.ReadLine());
+
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("Y value: ");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+int _yValue = Convert.ToInt32(Console.ReadLine());
