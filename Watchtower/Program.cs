0df0bfc4855a95b5d7c6ec9d338b581effb614d5
Justin Beam <justinbeam@MacBookAir.attlocal.net>
2025-03-02 09:20:42 -0600

    added comments to watchtower

--- Watchtower/Program.cs
+++ Watchtower/Program.cs
@@ -1,16 +1,21 @@
-﻿Console.BackgroundColor = ConsoleColor.White;
+﻿// console prints black text on white background
+Console.BackgroundColor = ConsoleColor.White;
 Console.ForegroundColor = ConsoleColor.Black;
 Console.Write("X value: ");
 
+// colors are reversed for user input
+/* the ReadLine() makes it color the entire next line */
 Console.BackgroundColor = ConsoleColor.Black;
 Console.ForegroundColor = ConsoleColor.White;
 int _xValue = Convert.ToInt32(Console.ReadLine());
 
-
+// console prints black text on white background
+// after console prints the rest of the line will be the color set by the previous ReadLine()
 Console.BackgroundColor = ConsoleColor.White;
 Console.ForegroundColor = ConsoleColor.Black;
 Console.Write("Y value: ");
 
+// user input again. back to default colors to keep it tidy
 Console.BackgroundColor = ConsoleColor.Black;
 Console.ForegroundColor = ConsoleColor.White;
 int _yValue = Convert.ToInt32(Console.ReadLine());
