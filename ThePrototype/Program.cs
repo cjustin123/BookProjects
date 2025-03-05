e417abcec120e40bf83e1340f539db030fa3d919
Justin Beam <justinbeam@MacBookAir.attlocal.net>
2025-03-05 11:26:41 -0600

    added do/while

--- ThePrototype/Program.cs
+++ ThePrototype/Program.cs
@@ -1,3 +1,9 @@
 ﻿
 Console.Title = "The Prototype";
-
+int pilotNumber;
+do
+{
+    Console.Write("Pilot, pick a number between 0 and 100: ");
+    pilotNumber = Convert.ToInt32(Console.ReadLine());
+}
+while (pilotNumber < 0 || pilotNumber > 100);
