5e27975bf7397f2a89aec5f26d970b463bd37b09
Justin Beam <justinbeam@MacBookAir.attlocal.net>
2025-03-30 15:13:19 -0500

    ass

---
+++ TheMagicCannon/Program.cs
@@ -0,0 +1,32 @@
+﻿
+for (int i = 1;i <= 100;i++)
+{
+    if ( i % 5 == 0 && i % 3 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.Write($"{i}: Combined");
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("\n");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write($"{i}: Electric");
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("\n");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Write($"{i}: Fire");
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("\n");
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write($"{i + 1}: Normal");
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("\n");
+    }
+}
