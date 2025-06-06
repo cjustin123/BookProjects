4db0e3ea5b7de840648978efbf5b98b0b3736ff2
CJustin123 <cjustinbeam@gmail.com>
2025-06-06 18:07:05 -0500

    map class added

---
+++ TheFountainOfObjects/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("HI WORL");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine(" ");
+Map _map = new Map();
+_map.DisplayMap();
+Console.ReadKey();
+
+
+
+
+
+
+/// END OF TOP LEVEL STATEMENT/ PROGRAM.MAIN() FUNCTION
+public enum RoomType { Entrance, Fountain, Empty }
+
+
+public class Room
+{
+    public RoomType RoomType { get; private set; }
+}
+public class Map
+{
+    public int Size { get; private set; } = 4;
+
+    public RoomType[,] RoomArray;
+    public Map()
+    {
+        RoomArray = new RoomType[Size, Size];
+        for (int x = 0; x < RoomArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < RoomArray.GetLength(1); y++)
+            {
+                RoomArray[x, y] = RoomType.Empty;
+            }
+        }
+    }
+    public void DisplayMap()
+    {
+        for (int x = 0; x < RoomArray.GetLength(0); x++)
+        {
+            Console.WriteLine("");
+            for (int y = 0; y < RoomArray.GetLength(1); y++)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write(RoomArray[x, y]);
+
+                if (y == RoomArray.GetLength(1) - 1)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    continue;
+                }
+                else
+                {
+
+                    Console.Write(", ");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+            }
+        }
+    }
+    
+}
--- ThePrototype/Program.cs
+++ ThePrototype/Program.cs
@@ -1,9 +1,32 @@
 ﻿
 Console.Title = "The Prototype";
 int pilotNumber;
+int hunterNumber;
 do
 {
     Console.Write("Pilot, pick a number between 0 and 100: ");
     pilotNumber = Convert.ToInt32(Console.ReadLine());
 }
 while (pilotNumber < 0 || pilotNumber > 100);
+
+Console.Clear(); 
+
+do 
+{
+    Console.Write("Hunter, guess a number: ");
+    hunterNumber = Convert.ToInt32(Console.ReadLine());
+    if ( hunterNumber > pilotNumber)
+    {
+        Console.Write($"{hunterNumber} is too high.");
+    }
+    else if (hunterNumber < pilotNumber)
+    {
+        Console.Write($"{hunterNumber} is too low.");
+    }
+    else
+    {
+        Console.Write("And boom goes the dynamite.");
+    }
+    Console.Write("\n");
+}
+while (hunterNumber != pilotNumber);
