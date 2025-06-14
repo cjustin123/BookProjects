301d73167e20ec52496233059cfe3c75ffa8dbb0
Justin <Justin@DESKTOP-UDRAI6E>
2025-06-14 10:06:01 -0500

    added a BuildLevel function to Map class

--- TheFountainOfObjects/Program.cs
+++ TheFountainOfObjects/Program.cs
@@ -8,6 +8,7 @@ _game.Run();
 public class Game
 {
     Map _map = new Map();
+    bool _isFountainActive = false;
 
     public void Run()
     {
@@ -16,10 +17,12 @@ public class Game
 }
 
 public struct Position { public int X, Y; }
-public enum RoomType { Entrance, Fountain, Empty }
+public enum RoomType { Entrance, Fountain, Empty, Wall }
 
 public class Room
 {
+    public RoomType North, South, East, West;
+    // MAYBE ABSTRACT
     public Room(int x, int y)
     {
         Position.X = x;
@@ -27,6 +30,11 @@ public class Room
     }
     public RoomType RoomType { get; private set; }
     public readonly Position Position;
+
+    public void SetRoomType(RoomType roomType)
+    {
+        RoomType = roomType;
+    }
 }
 public class Map
 {
@@ -36,28 +44,52 @@ public class Map
     public Map()
     {
         RoomArray = new Room[Size, Size];
+        BuildLevel();
+
+    }
+    private void BuildLevel()
+    {
+        // fountain placement
+        Random rnd = new Random();
+        int fountainX = rnd.Next(1, Size);
+        int fountainY = rnd.Next(1, Size);
+        Position fountainPosition = new Position();
+        fountainPosition.X = fountainX;
+        fountainPosition .Y = fountainY;
+
         for (int x = 0; x < RoomArray.GetLength(0); x++)
         {
             for (int y = 0; y < RoomArray.GetLength(1); y++)
             {
                 Room newRoom = new Room(x, y);
+                if (newRoom.Position.Equals(fountainPosition))
+                {
+                    newRoom.SetRoomType(RoomType.Fountain);
+                }
+                else if (x == 0 && y == 0)
+                {
+                    newRoom.SetRoomType(RoomType.Entrance);
+                }
+                else
+                {
+                    newRoom.SetRoomType(RoomType.Empty);
+                }
+                
                 RoomArray[x, y] = newRoom;
-      
-
             }
         }
-
     }
     ///////////////////////////////////////////////////////////////////
     public void DisplayMap()
     {
+        // MAKE IT DISPLAY Y AXIS IN REVERSE ORDER
         for (int x = 0; x < RoomArray.GetLength(0); x++)
         {
             Console.WriteLine("");
             for (int y = 0; y < RoomArray.GetLength(1); y++)
             {
 
-                Console.Write(RoomArray[x, y] + $"({x}, {y})");
+                Console.Write(RoomArray[x, y].RoomType + $"({x}, {y})");
                 if (y == RoomArray.GetLength(1) - 1)
                 {
 
