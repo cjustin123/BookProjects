4e9e664350de9013592529882c39ee9859cc5cb4
CJustin123 <cjustinbeam@gmail.com>
2025-06-14 08:33:53 -0500

    changed position to struct

--- TheFountainOfObjects/Program.cs
+++ TheFountainOfObjects/Program.cs
@@ -1,8 +1,5 @@
 ﻿
 
-
-
-
 Game _game;
 _game = new Game();
 _game.Run();
@@ -18,27 +15,35 @@ public class Game
     }
 }
 
-public record Position { public int x; public int y; } //MAKE A STRUCT
+public struct Position { public int X, Y; }
 public enum RoomType { Entrance, Fountain, Empty }
 
 public class Room
 {
+    public Room(int x, int y)
+    {
+        Position.X = x;
+        Position.Y = y;
+    }
     public RoomType RoomType { get; private set; }
-    public Position Position { get; private set; }
+    public readonly Position Position;
 }
 public class Map
 {
     public int Size { get; private set; } = 4;
 
-    public RoomType[,] RoomArray;
+    public Room[,] RoomArray;
     public Map()
     {
-        RoomArray = new RoomType[Size, Size];
+        RoomArray = new Room[Size, Size];
         for (int x = 0; x < RoomArray.GetLength(0); x++)
         {
             for (int y = 0; y < RoomArray.GetLength(1); y++)
             {
-                RoomArray[x, y] = RoomType.Empty;
+                Room newRoom = new Room(x, y);
+                RoomArray[x, y] = newRoom;
+      
+
             }
         }
 
@@ -52,7 +57,7 @@ public class Map
             for (int y = 0; y < RoomArray.GetLength(1); y++)
             {
 
-                Console.Write(RoomArray[x, y]);
+                Console.Write(RoomArray[x, y] + $"({x}, {y})");
                 if (y == RoomArray.GetLength(1) - 1)
                 {
 
